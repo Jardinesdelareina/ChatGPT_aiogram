b2ad750671a891543d662db6f1afc9044d35bf2c
jardinesdelareina <jardinesdelareina@mail.ru>
2023-03-10 15:57:32 +0300

    gpt-turbo

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ openai.api_key = env('OPENAI_TOKEN')
 bot = Bot(env('TELETOKEN'))
 dp = Dispatcher(bot)
 
+messages = [
+    {'role': 'system', 'content': 'You are a programmer and your job is to help you learn how to program and help you write code.'},
+    {'role': 'user', 'content': 'I am a beginner programmer and I need your help in learning programming and writing code'},
+    {'role': 'assistant', 'content': 'Greetings! How can I help?'},
+]
+
+def update(messages, role, content):
+    messages.append({'role': role, 'content': content})
+    return messages
+
 @dp.message_handler()
 async def chat(message: types.Message):
-    response = openai.Completion.create(
-        model='text-davinci-003',
-        prompt=message.text,
-        temperature=0.9,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=['You:']
+    update(messages, 'user', message.text)
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages 
     )
-
-    await message.answer(response['choices'][0]['text'])
+    await message.answer(response['choices'][0]['message']['content'])
 
 executor.start_polling(dp, skip_updates=True)
